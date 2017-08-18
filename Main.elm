3d3f2ff58a712a504f761c9439e3b7e88bf28364
entcheva <christina@thoughtbot.com>
2017-08-18 10:11:01 -0400

    Add fruitList

--- Main.elm
+++ Main.elm
@@ -2,6 +2,19 @@ module Main exposing (main)
 
 import Html exposing(..)
 
+fruitList : List String
+fruitList =
+  [ "apple"
+  , "banana"
+  , "strawberry"
+  , "peach"
+  , "melon"
+  ]
+
+dropdownItem : String - > Html a
+dropdownItem fruit =
+  li [] [ text fruit ]
+
 main : Html a
 main =
   text "Hello World"
